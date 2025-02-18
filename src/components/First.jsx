dc3acf2fe6c95412b556b08d4cdad1e1c8ee0e7b
jack <mrn47989@gmail.com>
2025-02-18 15:21:47 +0530

    newp

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -5,9 +5,9 @@ const First = () => {
     <div className=' flex w-full h-screen bottom-1/10 bg-[url("/bi.png")] bg-cover  bg-center   '>
     <div className='flex absolute w-full'>
         <h1 className='text-3xl text-orange-400 font-extrabold flex absolute text-justify mx-80 '> Zerox Point – Simplifying Documents, Optimizing Pricing, Empowering Efficiency</h1>
-        <div className=' w-200 h-140 flex absolute mx-40 my-50  rounded-2xl tranfrom translate-x-1/50  '>
+        <div className=' w-200 h-fit flex absolute mx-40 my-50  tranfrom translate-x-1/50  '>
            
-            <p className='p-3 text-2xl text-justify '> Welcome to Zerox Point, your ultimate solution
+            <p className='p-3 text-2xl text-justify bg-white-20 backdrop-blur-2xl rounded-3xl '> Welcome to Zerox Point, your ultimate solution
                for streamlined document management and pricing optimization. 
                Designed to cater to both administrators and vendors, Zerox Point ensures seamless 
                collaboration and effortless control over operations. Whether you're managing data, 
